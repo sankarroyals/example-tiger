6cc6cc5b5765401f537b56c726b52595715f70d1
sankar mukesh <ysankarmukesh@gmail.com>
2022-05-11 11:19:20 +0530

    today-2

--- react/sankar-example-react/src/components/Home.js
+++ react/sankar-example-react/src/components/Home.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-       
+       <h1>Click Here To Go ABout </h1>
+       <h3>
+            <a href="/about">About</a>
+       </h3>
 
 
     </div>
